bb00af66f43aa2e0b814dccca022eb52c7751892
xiongchuanyu <18811742305@163.com>
2020-05-05 13:26:38 +0800

    feat: helllo-word 尝试，debugger有问题github上官方也没有方案，先不降级react-native了

--- App.js
+++ App.js
@@ -1,46 +1,35 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  Image
 } from 'react-native';
-
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
   return (
-    <>
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header/>
-          <View style={styles.engine}>
-            <Text style={styles.footer}>测试</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
+     <View style={styles.sectionContainer}>
+         <Text style={styles.sectionTitle}>边寒爽和傻熊</Text>
+        <Image
+          style={{width: '100%', height: 300, marginTop: 20, marginBottom: 20}}
+          source={{uri: 'https://graph.baidu.com/resource/121cfa2e5611e7c416d9301588656087.jpg'}}
+        />
+         <Text style={styles.sectionDescription}>大家好，我是傻边。</Text>
+         <Text style={styles.sectionDescription}>我是一个小傻子，哈哈啊哈～</Text>
+         <Text style={styles.sectionDescription}>我很幸运遇到我熊哥。</Text>
+         <Text style={styles.sectionDescription}>我发誓我以后做饭洗碗好好做家务。</Text>
+         <Text style={styles.sectionDescription}>做一个没心没肺的小傻子。</Text>
+         <Text style={styles.sectionDescription}>对我熊哥服服帖帖的。</Text>
+         <Text style={styles.sectionDescription}>啊哈哈哈----------------</Text>
+     </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
@@ -53,8 +42,8 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
   },
   sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    marginTop: 50,
+    paddingHorizontal: 30,
   },
   sectionTitle: {
     fontSize: 24,
@@ -79,5 +68,3 @@ const styles = StyleSheet.create({
     textAlign: 'right',
   },
 });
-
-export default App;
